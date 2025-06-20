a0c393e489c579e74584f01eddb5df772e793c30
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 23:51:09 +0000

    [automatic commit] 2025-06-20 23:51:09 UTC 203.214.144.13

--- part1/app.js
+++ part1/app.js
@@ -4,12 +4,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 let db;
 
