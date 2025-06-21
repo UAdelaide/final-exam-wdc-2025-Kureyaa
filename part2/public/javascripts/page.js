a55dc24823180cfd60114bc5c7a5cf2e5eb0fe49
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 04:24:29 +0000

    [automatic commit] 2025-06-21 04:24:29 UTC 203.214.144.13

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -211,7 +211,6 @@ function logout(){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            sessionStorage.removeItem('currentUser');
             window.location.href = '/';
         } else {
             alert("Logout failed");
